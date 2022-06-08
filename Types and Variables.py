a9fd3452daea4ab68bc4015841d06ae27afccf5b
Bianca Bringel <bbringel@outlook.com>
2022-06-08 11:27:16 -0400

    Add files via upload

---
+++ Types and Variables.py
@@ -0,0 +1,20 @@
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+bmi = weight / (height ** 2)
+new_bmi = round(bmi)
+
+if new_bmi < 18.5:
+    print(f"Your BMI is {new_bmi}, you are underweight.")
+elif new_bmi < 25:
+    print(f"Your BMI is {new_bmi}, you have a normal weight.")
+elif new_bmi < 30:
+    print(f"Your BMI is {new_bmi}, you are slightly overweight.")
+elif new_bmi < 35:
+    print(f"Your BMI is {new_bmi}, you are obese.")
+else:
+    print(f"Your BMI is {new_bmi}, you are clinically obese.")
+
+
+
+
